e16ab097ece206065a8b01f242d553886401d377
DaudCh <daudch42@gmail.com>
2025-04-03 15:33:49 +0500

    Add Jouranl Entry Form

---
+++ JournalUI/AddJournalEntryWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using JournalSystem.Core.Services;
+using System.Windows;
+
+namespace JournalUI
+{
+    public partial class AddJournalEntryWindow : Window
+    {
+        private readonly IJournalEntryService _journalEntryService;
+
+        public AddJournalEntryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Journal Entry Saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ JournalUI/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using JournalSystem.Repositories.Context;
+using JournalSystem.Services;
+using JournalSystem.Core.Services;
+using JournalSystem.Core.Repositories;
+using JournalSystem.Repositories;
+using JournalUI;
+
+namespace JournalUI
+{
+    public partial class App : Application
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+        private IHost _host;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    string connectionString = "Data Source=DESKTOP-4F8B9BO\\MSSQLSERVER01;Initial Catalog=JournalDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(connectionString));
+
+                    // Register services and repositories
+                    services.AddScoped<IJournalEntryRepository, JournalEntryRepository>();
+                    services.AddScoped<IJournalEntryService, JournalEntryService>();
+                    services.AddScoped<IJournalLineRepository, JournalLineRepository>();
+                    services.AddScoped<IJournalLineService, JournalLineService>();
+                    services.AddScoped<IAccountRepository, AccountRepository>();
+                    services.AddScoped<IAccountService, AccountService>();
+                    services.AddScoped<ICostCenterRepository, CostCenterRepository>();
+                    services.AddScoped<ICostCenterService, CostCentreService>();
+                    services.AddScoped<ICurrencyRepository, CurrencyRepository>();
+                    services.AddScoped<ICurrencyService, CurrencyService>();
+                    services.AddScoped<IDimensionRepository, DimensionRepository>();
+                    services.AddScoped<IDimensionService, DimensionService>();
+
+                    services.AddAutoMapper(typeof(MappingProfile));
+
+
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+
+            await _host.StartAsync();
+
+            ServiceProvider = _host.Services;
+            base.OnStartup(e);
+
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+    }
+}
+
